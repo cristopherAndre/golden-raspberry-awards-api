ffd95dcabf619f63a87552fde43f9f0d9d148d75
Cristopher André <cristopher@weg.net>
2022-10-27 14:22:18 -0300

    Fix StudioDTO

--- src/main/java/com/cristopherandre/goldenraspberryawardsapi/dto/StudioDTO.java
+++ src/main/java/com/cristopherandre/goldenraspberryawardsapi/dto/StudioDTO.java
@@ -2,11 +2,13 @@ package com.cristopherandre.goldenraspberryawardsapi.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 /**
  * @author Cristopher Andre
  */
+@Data
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
